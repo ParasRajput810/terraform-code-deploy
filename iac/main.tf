560955a831ce0dc1011226d721f0d2e0fae32f09
Gourav Kumar <paraswork810@gmail.com>
2025-01-09 22:33:25 +0530

    ritik super code

--- iac/main.tf
+++ iac/main.tf
@@ -19,10 +19,10 @@ resource "azurerm_app_service" "my_app_service_plan" {
   location = azurerm_resource_group.mRG.location
   resource_group_name = azurerm_resource_group.mRG.name
 
-  site_config {
-    use_32_bit_worker_process = true  # Enable 32-bit worker processes
-    scm_type = "GitHub"
-  }
+#   site_config {
+#     use_32_bit_worker_process = true  # Enable 32-bit worker processes
+#     scm_type = "GitHub"
+#   }
 }
 
 
